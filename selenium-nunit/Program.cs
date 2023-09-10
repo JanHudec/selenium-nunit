a2fdfa6ab2b281a738de98535fb18162935c80b3
Jan Hudec <xhudec1@fi.muni.cz>
2023-09-10 17:18:08 +0200

    Initial commit

---
+++ selenium-nunit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace selenium_nunit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
