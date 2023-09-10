044796ceb2ad41b7dd11c8a793d0790b69cf9ff3
Jan Hudec <xhudec1@fi.muni.cz>
2023-09-10 17:27:41 +0200

    Add logger class

---
+++ selenium-nunit/Logger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium_nunit
+{
+    public static class Logger
+    {
+        // Here you can change the logging file location or name
+        private static string logFilePath = "../../../log.txt";
+
+        public static void LogMessage(string message)
+        {
+            try
+            {
+                using (StreamWriter writer = File.AppendText(logFilePath))
+                {
+                    DateTime currentTime = DateTime.Now;
+
+                    string logEntry = $"{currentTime.ToString("yyyy-MM-dd HH:mm:ss")} - {message}";
+
+                    writer.WriteLine(logEntry);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while logging the message: {ex.Message}");
+            }
+        }
+
+        public static void NewSession()
+        {
+            try
+            {
+                using (StreamWriter writer = File.AppendText(logFilePath))
+                {
+                    writer.WriteLine("------starting a new session------");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while logging the message: {ex.Message}");
+            }
+        }
+    }
+}
--- selenium-nunit/Program.cs
+++ selenium-nunit/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Program is running!");
+
+            Logger.NewSession();
+            Logger.LogMessage("Testing message");
+
+            Console.WriteLine("Program finished.");
         }
     }
 }
